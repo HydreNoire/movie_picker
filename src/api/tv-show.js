87590e1f1a867c8c3301b5060fcfc9adb6ba5969
Lise <lise692@gmail.com>
2023-10-29 22:14:22 +0100

    moove param

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,28 +1,24 @@
-import { API_KEY_PARAM, BASE_URL } from "../config";
+import { BASE_URL } from "../config";
 
 export class TVShowAPI {
     static async fetchPopulars() {
-        const response = await fetch(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
+        const response = await fetch(`${BASE_URL}tv/popular${process.env.REACT_APP_API_KEY_PARAM}`);
         const shows = await response.json();
 
-        // console.log(movies.results);
         return shows.results;
     }
 
     static async fetchRecommendations(tvShowId) {
-        const response = await fetch(`${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`);
+        const response = await fetch(`${BASE_URL}tv/${tvShowId}/recommendations${process.env.REACT_APP_API_KEY_PARAM}`);
         const recommendationShows = await response.json();
 
-        // console.log(recommendationShows.results);
         return recommendationShows.results;
     }
 
     static async fetchByTitle(title) {
-        const response = await fetch(`${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`);
+        const response = await fetch(`${BASE_URL}search/tv${process.env.REACT_APP_API_KEY_PARAM}&query=${title}`);
         const searchingShow = await response.json();
 
-        console.log("***", searchingShow);
-        // console.log(recommendationShows.results);
         return searchingShow.results;
     }
 }
--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ const API_KEY_PARAM = "?api_key=1b0db27fde52f8eba04e106cb789b32b";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMG_BASE_URL = "https://image.tmdb.org/t/p/w300/";
 
-export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_BASE_URL };
+export { BASE_URL, BACKDROP_BASE_URL, SMALL_IMG_BASE_URL };
